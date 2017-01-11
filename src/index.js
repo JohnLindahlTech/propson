694bb66620422e9ca016500eea0411a5d52228eb
John Bäckström <john@johnphoto.se>
2017-01-11 04:44:27 +0100

    Fixed lint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import propson from './propson';
 import contract from 'neat-contract';
+import propson from './propson';
 
-export default function Propson(sources, dir, isNested){
+export default function Propson(sources, dir, isNested) {
   contract('sources', [String, RegExp], sources);
   contract('dir', String, dir);
-  const _name = isNested ? 'nested' : 'flat';
-  return propson(sources, {_name, dir });
+  const _name = isNested ? 'nested' : 'flat'; // eslint-disable-line no-underscore-dangle
+  return propson(sources, { _name, dir });
 }
 
 export { default as convertPropsToFlatJson } from './convertPropsToFlatJson';
